21bdfbe4026e8de2af0cae6c8f3c1951b5c330b2
Er0nax <eronax007@gmail.com>
2021-07-01 15:35:03 +0200

    Coming Soon Page added

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+// This is the start of the website. 
+// You will be redirected to /views/index.view.php
+// Do not change the link or else it will not work.
+
+require '../views/index.view.php';
---
+++ views/index.view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Project LFM</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="../views/css/index.css" media="screen">
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Project LFM
+            </div>
+
+            <div class="links">
+                <a href="https://discord.gg/kefsJANSPD">Discord</a>
+                <a href="https://github.com/Er0nax/project-lfm">Github</a>
+                <a href="https://lfm.statuspage.io/">Status</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
